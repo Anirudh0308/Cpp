4b70c962f1ee14ccd43b4ea0cabf77c93de7be16
Anirudh0308 <129490209+Anirudh0308@users.noreply.github.com>
2024-08-09 10:33:21 +0530

    Add files via upload

---
+++ bitwise.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+   //bitwise operators ( &,|,~ and,or,not)
+    cout<<(10&4)<<endl;
+    cout<<(11&9)<<endl;
+    cout<<(10|4)<<endl;
+    cout<<(11|4)<<endl;
+    cout<<(~10)<<endl;
+    cout<<(10^4)<<endl;
+    cout<<(12^5)<<endl<<endl;
+//left shift
+    cout<<(9<<1)<<endl;
+    cout<<(7<<1)<<endl;
+    cout<<(9<<2)<<endl;
+//right shift
+    cout<<(2>>1)<<endl;
+    cout<<(8>>2)<<endl;
+
+    return 0;
+}
